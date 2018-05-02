7993beb6af71f6b5c5d6ddac6c57eda16e710c95
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2018-05-01 20:31:02 -0400

    use cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import expressGraphQL from 'express-graphql';
 import { printSchema } from 'graphql';
 import schema from './graphqlSchema';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/graphql/schema', (req, res) => {
   res.type('text/plain').send(printSchema(schema));
