12430cddd9a652d62ad16fca9b3accc3cc1bc0dd
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2018-05-07 22:08:55 -0400

    Search by location for bus stops.

--- src/graphqlSchema/park/BusType.js
+++ src/graphqlSchema/park/BusType.js
@@ -3,24 +3,21 @@ import {
   GraphQLNonNull,
   GraphQLInt,
   GraphQLString,
-  GraphQLList,
   GraphQLFloat,
 } from 'graphql';
-
-//   stopabbr, stopid, lon, lat
 
 export default new GraphQLObjectType({
   name: 'Bus',
   description: 'This object represents bus type.',
   fields: {
-    objectid: { type: new GraphQLNonNull(GraphQLInt) },
-    stopname: { type: new GraphQLNonNull(GraphQLString) },
-    routes: { type: new GraphQLNonNull(GraphQLString)},
-    shelter: { type: new GraphQLNonNull(GraphQLString)},
-    bench: { type: new GraphQLNonNull(GraphQLString)},
-    stopabbr: { type: new GraphQLNonNull(GraphQLString)},
-    stopid: { type: new GraphQLNonNull(GraphQLString)},
-    lat: { type: new GraphQLNonNull(GraphQLFloat)},
-    lon: { type: new GraphQLNonNull(GraphQLFloat)}
+    objectId: { type: new GraphQLNonNull(GraphQLInt) },
+    stopName: { type: new GraphQLNonNull(GraphQLString) },
+    routes: { type: new GraphQLNonNull(GraphQLString) },
+    shelter: { type: new GraphQLNonNull(GraphQLString) },
+    bench: { type: new GraphQLNonNull(GraphQLString) },
+    stopAbbr: { type: new GraphQLNonNull(GraphQLString) },
+    stopId: { type: new GraphQLNonNull(GraphQLString) },
+    lat: { type: new GraphQLNonNull(GraphQLFloat) },
+    lon: { type: new GraphQLNonNull(GraphQLFloat) },
   },
 });
--- src/graphqlSchema/park/busQueries.js
+++ src/graphqlSchema/park/busQueries.js
@@ -1,13 +1,33 @@
-import { GraphQLList, GraphQLString } from 'graphql';
+import { GraphQLList, GraphQLString, GraphQLFloat } from 'graphql';
 import _ from 'lodash';
 import BusType from './BusType';
 
 const request = require('request');
+const geolib = require('geolib');
 
 const options = {
   method: 'GET',
   url: 'https://data.delaware.gov/resource/n5hx-5mgi.json',
 };
+
+function toBusTypesArray(body) {
+  const a = JSON.parse(body);
+  return a.map(val => {
+    const b = {
+      objectId: val.objectid,
+      stopName: val.stopname,
+      routes: val.routes,
+      shelter: val.shelter,
+      bench: val.bench,
+      stopAbbr: val.stopabbr,
+      stopId: val.stopid,
+      lat: val.shape.latitude,
+      lon: val.shape.longitude,
+    };
+
+    return b;
+  });
+}
 
 const busStops = {
   type: new GraphQLList(BusType),
@@ -15,26 +35,50 @@ const busStops = {
   args: {
     stopId: { type: GraphQLString },
     stopName: { type: GraphQLString },
+    latitude: { type: GraphQLFloat },
+    longitude: { type: GraphQLFloat },
+    distance: { type: GraphQLFloat },
   },
-  resolve: (parent, args) => {
-    const stops = new Promise((resolve, reject) => {
+  resolve: (parent, args) =>
+    new Promise((resolve, reject) => {
       request(options, (error, response, body) => {
         if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body));
+          return reject(error);
         }
+
+        const stops = toBusTypesArray(body);
+        if (args.stopId) {
+          return resolve(_.filter(stops, { stopId: args.stopId }));
+        } else if (args.stopName) {
+          return resolve(
+            _.filter(stops, s => _.includes(s.stopName, args.stopName)),
+          );
+        } else if (args.latitude && args.longitude) {
+          const distance = args.distance || 10;
+          const l1 = {
+            latitude: args.latitude,
+            longitude: args.longitude,
+          };
+
+          const r = [];
+          for (let i = 0; i < stops.length; i += 1) {
+            const l2 = {
+              latitude: stops[i].lat,
+              longitude: stops[i].lon,
+            };
+
+            const d = geolib.getDistance(l1, l2);
+            if (d <= distance) {
+              r.push(stops[i]);
+            }
+          }
+
+          return resolve(r);
+        }
+
+        return resolve(stops);
       });
-    });
-
-    if (args.stopId) {
-      return _.filter(stops, { stopId: args.stopId });
-    } else if (args.stopName) {
-      return _.filter(stops, s => _.includes(s.stopName, args.stopName));
-    }
-
-    return stops;
-  },
+    }),
 };
 
 export default {
